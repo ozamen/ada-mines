43947132b72349f9fcbf0a8c9543b7003ab9893a
Antoine Coulon <antoine.coulon@epita.fr>
2021-01-28 14:24:27 +0100

    ok double array

--- main.adb
+++ main.adb
@@ -1,9 +1,21 @@
 with Ada.Text_IO;
+use Ada.Text_IO;
 
-procedure Hello_World is
+procedure main is
+    type My_Int is range 0 .. 1000;
+    type col is range 1 .. 4;
+    type line is range 1 .. 4;
+    type My_Int_Array is array (col, line) of My_Int;
+    Arr : My_Int_Array := ((3, 6, 7, 1), (5, 1, 2, 7), (5, 1, 2, 7), (5, 1, 2, 7));
 begin
    Ada.Text_IO.Put_Line ("Welcome to ADA Minesweeper v1.0.0");
 
-
-
-end Hello_World;
+   for i in col loop
+       Put("| ");
+       for j in line loop
+            Put(My_Int'Image (Arr (i, j)));
+            Put(" | ");
+        end loop;
+        New_Line;
+   end loop;
+end main;
