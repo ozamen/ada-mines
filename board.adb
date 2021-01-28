f2b5154d477de9808680ec7bdd8461b68443401b
Antoine Coulon <antoine.coulon@epita.fr>
2021-01-28 18:04:47 +0100

    random near OK

--- board.adb
+++ board.adb
@@ -1,20 +1,37 @@
-package body Board is
 
+
+package body Board is
+   -- PRINT THE CONSOLE BOARD
    procedure DumpBoard(Board : Array2D) is
    begin
-      for i in Col loop
+      for i in Line loop
           Put("| ");
-          for j in Line loop
-              Put(Status'Image (Board (i, j)));
-               Put(" | ");
+          for j in Col loop
+              Put(Status'Image (Board (j, i)));
+              Put(" | ");
           end loop;
           New_Line;
       end loop;
    end DumpBoard;
 
+   -- GENERATE THE BOARD WITH THE FIRST CLICK POSITION OF THE USER
    function generateBoard(c : Col; l : Line) return Array2D is
-       Board : Array2D := ((3, 2, 3, 1), (5, 1, 2, 0), (5, 1, 2, 2), (5, 1, 2, 1));
+       Board : Array2D := ((others => (others => 0)));
+       subtype Rand_Range is Integer range 0 .. 10;
+       package Rand_Int is new Ada.Numerics.Discrete_Random(Rand_Range);
+       use Rand_Int;
+       gen : Rand_Int.Generator;
+       ret_val: Rand_Range;
    begin
+       for i in 0 .. 10 loop
+            Rand_Int.Reset(gen);
+            ret_val := Random(gen);
+            Put(ret_val);
+
+            Put(ret_val);
+            --Board (ret_val, 3) := 1;
+       end loop;
+
        return Board;
    end generateBoard;
 
--- board.ads
+++ board.ads
@@ -1,10 +1,15 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Numerics.discrete_Random;
 
 package Board is
 
+    Height: constant Integer := 15;
+    Width: constant Integer := 15;
+
     type Status is range 0 .. 5;
-    type Col is range 1 .. 4;
-    type Line is range 1 .. 4;
+    type Col is range 1 .. Width;
+    type Line is range 1 .. Height;
     type Array2D is array (Col, Line) of Status;
 
    procedure DumpBoard(Board : Array2D);
